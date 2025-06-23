d42c457ce0e7360585ef4b861fdb3ba3041767d2
GusTheAnt <gustheant@live.com>
2025-06-23 10:34:07 -0500

    Removed `crypto-js`, migrated to `node:crypto`
    Updated `@types/node` package, removed `crypto-js` package import in `helpers.ts` in favor of native crypto module.

--- packages/iracing-api/src/helpers.ts
+++ packages/iracing-api/src/helpers.ts
@@ -1,6 +1,20 @@
-import CryptoJS from 'crypto-js'
+const { createHash } = await import('node:crypto');
 
-export const encryptPassword = (email: string, password: string) =>
-    CryptoJS.enc.Base64.stringify(
-        CryptoJS.SHA256(password + email.toLowerCase())
-    )
+export const encryptPassword = (email: string, password: string) => {
+    const hash = createHash('sha256')
+        .update(password + email.toLowerCase())
+        .digest('base64')
+    
+    return hash
+}
+
+// import CryptoJS from 'crypto-js'
+
+// crypto-js is no longer maintained, dev recommends using native `crypto` module in Node
+// https://github.com/brix/crypto-js
+// https://nodejs.org/docs/latest/api/crypto.html#class-hash
+
+// export const encryptPassword = (email: string, password: string) =>
+//     CryptoJS.enc.Base64.stringify(
+//         CryptoJS.SHA256(password + email.toLowerCase())
+//     )
